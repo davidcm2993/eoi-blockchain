04243a6f6c2489bd2161f49648b4acd52983e716
Javier Aguirre <contacto@javaguirre.net>
2021-11-09 10:24:26 +0100

    Adding dashboard

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set('view engine', 'handlebars')
 const port = process.env.PORT || 3000
 
 function isAuthenticated(user, password) {
+    // TODO Comprobar en base de datos el usuario
     return user == 'admin' && password == 'admin'
 }
 
@@ -22,6 +23,10 @@ app.get('/', function(request, response) {
 
 // POST Cuando enviamos un form (Login) /login
 // Un pagina de login
+// Cuando alguien meta user "admin" y password "admin"
+// 1. decirme que estoy autenticado "EXITO!"
+// 2. redireccione a una pagina interna /dashboard
+// 3. si el user password no es admin admin, mostrar "ERROR!"
 app.get('/login', (request, response) => {
     response.render('login')
 })
@@ -40,10 +45,9 @@ app.post('/login', (request, response) => {
     }
 })
 
-// Cuando alguien meta user "admin" y password "admin"
-// 1. decirme que estoy autenticado "EXITO!"
-// 2. redireccione a una pagina interna /dashboard
-// 3. si el user password no es admin admin, mostrar "ERROR!"
+app.get('/dashboard', (request, response) => {
+    response.render('dashboard')
+})
 
 
 app.get('/contacto', function(request, response) {
@@ -57,7 +61,7 @@ app.post('/contacto', function(request, response) {
     response.send('Enviado')
 })
 
-app.get('/:user', function(request, response) {
+app.get('/users/:user', function(request, response) {
     // TODO Hacer una consulta para traerme los datos
     // de este usuario
     response.send(`Usuario ${request.params.user}`)
