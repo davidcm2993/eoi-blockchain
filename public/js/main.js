22b1837d831935cb115273e9cf6d828282dfb31b
Javier Aguirre <contacto@javaguirre.net>
2021-11-15 10:50:18 +0100

    Added Morgan

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,5 @@
-console.log('Hola')
-
 $('.delete-card-item').click(function() {
-    console.log('Clicado')
-    alert('Clicado')
-    console.log(this.data)
-    $('#delete-card-button').prop('href', '...') // Prop href + id que me viene del data
+    console.log($(this).data())
+    const cardId = $(this).data('card-id')
+    $('#delete-card-button').prop('href', `/delete_card/${cardId}`)
 })
