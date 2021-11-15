3cc473b0d3ef787f3976431a69c71d99d4ec184f
Javier Aguirre <contacto@javaguirre.net>
2021-11-15 11:27:28 +0100

    Updated css on card detail

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,24 @@ $('.delete-card-item').click(function() {
     const cardId = $(this).data('card-id')
     $('#delete-card-button').prop('href', `/delete_card/${cardId}`)
 })
+
+$('#create-card-button').click(function() {
+    console.log('Click')
+    const name = $('input[name="name"]').val()
+    const description = $('input[name="description"]').val()
+    const price = $('input[name="price"]').val()
+
+    // TODO Cuando estos valores sean vacios,
+    // Mostramos un mensaje de error y paramos el click
+    // Tener deshabilitado, y habilitarlo solo cuando hayamos metido
+    // todos los valores
+
+    // Cuando se le de a crear una vez, lo deshabilitamos para que no se
+    // le de dos veces
+})
+
+$('input[name="name"]').change(function() {
+    console.log('Cambio')
+    console.log($(this).val())
+    $('#create-card-button').prop('disabled', false)
+})
