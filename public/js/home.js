19b7b1248e8d606e5efd492b0d1b6133678579f4
Javier Aguirre <contacto@javaguirre.net>
2021-11-15 13:41:35 +0100

    crypto list

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,32 @@
+
+const showDogImage = function(imageUrl) {
+    let image = $(
+        '<img>'
+    ).prop('src', imageUrl).addClass('img-fluid')
+
+    $('#dog-img').append(image)
+}
+
+const showList = function(cryptoList) {
+    let cryptoHtmlList = $('<ul>')
+    let listItem
+
+    for(item of cryptoList) {
+        listItem = $('<li>').text(`${item.AssetLong} (${item.SystemProtocol})`)
+        cryptoHtmlList.append(listItem)
+    }
+
+    $('#crypto-list').append(cryptoHtmlList)
+}
+
 $.getJSON(
     'https://dog.ceo/api/breeds/image/random',
     function(data) {
-        console.log(data)
-        console.log(data.message)
+        showDogImage(data.message)
+})
+
+$.getJSON(
+    'https://nova.bitcambio.com.br/api/v3/public/getassets',
+    function(data) {
+        showList(data.result)
 })
